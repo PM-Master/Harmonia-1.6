dd87547c0e6a9fa21e353e206fcb665687ab3b3e
gopikatwala <gopi.katwala@nist.gov>
2017-01-26 10:55:03 -0500

    Removed unnecessary classes.

--- src/gov/nist/csd/pm/server/IPolicyMachineCommands.java
+++
@@ -1,7 +0,0 @@
-package gov.nist.csd.pm.server;
-
-public interface IPolicyMachineCommands {
-	void Connect(String clientID);
-	void Reset(String clientId, String sessionId);
-	//void getGraph(String clientId, String sGraphType, String sAnchorId, String sAnchorLabel, String sAnchorType, String sLevel);
-}
